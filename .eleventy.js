8c09827e71d4fdaeb4eef8332aee90d277312b87
narmeenk97 <144829367+narmeenk97@users.noreply.github.com>
2024-11-20 00:47:52 -0500

    Updates

--- .eleventy.js
+++ .eleventy.js
@@ -2,6 +2,7 @@ module.exports = function(eleventyConfig) {
   eleventyConfig.addPassthroughCopy('src/_data/assets/style.css');
   eleventyConfig.addPassthroughCopy('node_modules/bootstrap/dist'); 
   eleventyConfig.addPassthroughCopy('src/_data/pictures'); 
+  //eleventyConfig.addPassthroughCopy('src/_data/assets');
   return {
       dir: {
         input: "src"
