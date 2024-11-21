d7447ff6f9910d2738825ca213e5a4687f72e796
narmeenk97 <144829367+narmeenk97@users.noreply.github.com>
2024-11-21 12:35:32 -0500

    final

--- .eleventy.js
+++ .eleventy.js
@@ -4,6 +4,8 @@ module.exports = function(eleventyConfig) {
   eleventyConfig.addPassthroughCopy('src/_data/pictures'); 
   eleventyConfig.addPassthroughCopy('src/_data/scripts/themeSwitcher.js'); 
   eleventyConfig.addPassthroughCopy('src/_data/assets/Poster.pdf');
+  eleventyConfig.addPassthroughCopy('src/_data/assets/Bibliography.pdf');
+  eleventyConfig.addPassthroughCopy('src/_data/assets/Library List of Resources.pdf');
   return {
       dir: {
         input: "src"
